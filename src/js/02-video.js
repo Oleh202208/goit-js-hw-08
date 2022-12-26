f183b0c330b1692d6f6bbc4ffbce1b2f52a25a7b
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-12-26 13:53:31 +0100

    2+

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,23 @@
 import Player from '@vimeo/player';
-console.log(Player)
+import throttle from 'lodash.throttle';
+// console.log(throttle)
+
+const TIME_PLAY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const timePlayed = JSON.parse(localStorage.getItem(TIME_PLAY));
+
+player.on('timeupdate', throttle(getTimeupdate, 1000));
+
+function getTimeupdate(evt){
+  let storageTime = JSON.stringify(evt.seconds);
+  localStorage.setItem(TIME_PLAY, storageTime);
+}
+player.setCurrentTime(timePlayed).then(function(timePlayed) {}).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+
+    default:
+      break;
+  }});
