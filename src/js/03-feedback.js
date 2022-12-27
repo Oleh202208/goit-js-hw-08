fb65ba872b3dc9adbf9a6fd2dd68f44add87a485
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-12-27 12:24:06 +0100

    3+

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onClickButton);
+
+const KEY_LOCAL = 'feedback-form-state';
+const userData = {};
+saveInput();
+
+function onInput(evt) {
+  userData[evt.target.name] = evt.target.value;
+  localStorage.setItem(KEY_LOCAL, JSON.stringify(userData));
+}
+
+function saveInput () {
+  const saveData = JSON.parse(localStorage.getItem(KEY_LOCAL));
+  if(saveData) {
+Object.entries(saveData).forEach(([key, value]) => {
+  formEl[key].value = value;
+  userData[key] = value;
+});
+  }
+}
+
+function onClickButton(evt) {
+evt.preventDefault();
+evt.target.reset();
+
+console.log(JSON.parse(localStorage.getItem(KEY_LOCAL)));
+localStorage.removeItem(KEY_LOCAL);
+
+}
